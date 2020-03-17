d4636bc89533ef026952a4b8adf703ec58d1cbfd
Angel Rodríguez <angelmanuelrs@gmail.com>
2020-03-17 18:48:06 -0400

    Force actions to run

--- e2e/init.js
+++ e2e/init.js
@@ -2,7 +2,7 @@ const detox = require('detox');
 const config = require('../package.json').detox;
 const adapter = require('detox/runners/jest/adapter');
 const specReporter = require('detox/runners/jest/specReporter');
-
+ 
 // Set the default timeout
 jest.setTimeout(120000);
 jasmine.getEnv().addReporter(adapter);
